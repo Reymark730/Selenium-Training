d1b0b4d42b174c6a7182ad1518885ca78ddd5979
Reymark730 <reymark.calahi@gmail.com>
2026-01-30 21:38:07 +0800

    Add files via upload

---
+++ Phase1.java
@@ -0,0 +1,41 @@
+package com.bootcamp;
+
+import io.github.bonigarcia.wdm.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class Phase1 {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void getPageInfo() throws InterruptedException {
+        driver.get("https://the-internet.herokuapp.com");
+
+        String pageTitle = driver.getTitle();
+        System.err.println("Page Title: " + pageTitle);
+        Assert.assertEquals(pageTitle, "The Internet");
+
+        String pageUrl = driver.getCurrentUrl();
+        System.err.println("Page URL: " + pageUrl);
+        Assert.assertEquals(pageUrl, "https://the-internet.herokuapp.com/");
+        Thread.sleep(2000);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+}
---
+++ Phase2.java
@@ -0,0 +1,45 @@
+package com.bootcamp;
+
+import io.github.bonigarcia.wdm.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+public class Phase2 {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void loginInteraction() throws InterruptedException {
+        driver.get("https://the-internet.herokuapp.com/login");
+
+        // locate username input field
+        WebElement usernameInput = driver.findElement(By.name("username"));
+        usernameInput.sendKeys("tomsmith");
+
+        // locate password input field
+        WebElement passwordInput = driver.findElement(By.name("password"));
+        passwordInput.sendKeys("SuperSecretPassword!");
+
+        // locate login button
+        WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        loginButton.click();
+
+        Thread.sleep(2000);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+}
